bcbf1cd5fcd34b8dd68758228fd0c7a58bbaf219
FilipBaturan <filip.baturan@gmail.com>
2018-04-09 21:39:24 +0200

    Commit zbog izmena

--- Stocks/Stocks/UserControls/ItemSelection.xaml.cs
+++ Stocks/Stocks/UserControls/ItemSelection.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +19,49 @@ namespace Stocks.UserControls
     /// <summary>
     /// Interaction logic for ItemSelection.xaml
     /// </summary>
-    public partial class ItemSelection : UserControl
+    public partial class ItemSelection : UserControl, INotifyPropertyChanged
     {
+        private string _searchText;
+
+        public string SearchText
+        {
+            get { return _searchText; }
+            set
+            {
+                _searchText = value;
+
+                OnPropertyChanged("SearchText");
+                OnPropertyChanged("MyFilteredItems");
+            }
+        }
+
+        public List<string> MyItems { get; set; }
+
+        public IEnumerable<string> MyFilteredItems
+        {
+            get
+            {
+                Console.WriteLine(SearchText);
+                if (SearchText == null) return MyItems;
+                return MyItems.Where(x => x.ToUpper().StartsWith(SearchText.ToUpper()));
+            }
+        }
+
         public ItemSelection()
         {
             InitializeComponent();
+
+            MyItems = new List<string>() { "ABC", "DEF", "GHI" };
+
+            this.DataContext = this;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(name));
         }
     }
+    
 }
